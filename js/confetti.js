c0848df173994ee3e16c4fc74f0a82010e217870
Beomene <trinkaloop@gmail.com>
2026-03-10 22:59:54 +0100

    Create ConfettiManager class in js/confetti.js file

---
+++ js/confetti.js
@@ -0,0 +1,15 @@
+class ConfettiManager {
+    static explode(x, y) {
+        // Logic to create an explosion of confetti at (x, y)
+    }
+
+    static createConfetti(color) {
+        // Logic to create a single piece of confetti with the given color
+    }
+
+    static rainbowExplosion(x, y) {
+        // Create a rainbow explosion of confetti at (x, y)
+    }
+}
+
+export default ConfettiManager;
